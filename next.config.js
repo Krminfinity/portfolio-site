b6abafea04697f4213c8c55112ed2dd1ab3fdd80
Makoto Kuramochi <krmchi2015@gmail.com>
2025-08-23 08:58:40 +0900

    next.config.jsのbasePathとassetPrefixのコメントアウトを解除し、画像設定を維持

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio-site',
-  assetPrefix: '/portfolio-site/',
+  // basePath: '/portfolio-site',
+  // assetPrefix: '/portfolio-site/',
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
